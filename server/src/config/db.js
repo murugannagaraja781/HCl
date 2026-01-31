709fde8dc7af00da8745d0cfdac7c736475cdc6a
murugannagaraja781 <murugannagaraja781@gmail.com>
2026-01-31 19:52:35 +0530

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const uri = process.env.MONGO_URI || process.env.MONGODB_URI;
+
+        if (!uri) {
+            throw new Error('MongoDB URI is missing! Please set MONGO_URI in environment variables.');
+        }
+
+        const conn = await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
